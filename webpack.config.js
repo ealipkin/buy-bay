992e0dcee19758d294da30411ab34cbec57e174a
ealipkin <evgeny2@rambler.ru>
2020-10-23 00:33:51 +0300

    add fonts, add pug

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+import { VueLoaderPlugin } from 'vue-loader';
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        oneOf: [
+          // это применяется к `<template lang="pug">` в компонентах Vue
+          {
+            resourceQuery: /^\?vue/,
+            use: ['pug-plain-loader'],
+          },
+          // это применяется к импортам pug внутри JavaScript
+          {
+            use: ['raw-loader', 'pug-plain-loader'],
+          },
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+};
