d5f78b8934091e5f3255840c040b482936098b2c
2and1or3 <2and1or3@gmail.com>
2020-11-05 01:18:02 +0300

    Rename classes and add clipboard

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,13 @@ import './assets/fonts/opensans.css';
 import vueAwesomeCountdown from 'vue-awesome-countdown';
 import Paginate from 'vuejs-paginate';
 import VueTruncate from 'vue-truncate-filter';
+import VueClipboard from 'vue-clipboard2';
 
 import VModal from 'vue-js-modal';
 
 Vue.use(vueAwesomeCountdown, 'vac');
 Vue.use(VueTruncate);
+Vue.use(VueClipboard);
 
 Vue.config.productionTip = false;
 Vue.use(VModal);
